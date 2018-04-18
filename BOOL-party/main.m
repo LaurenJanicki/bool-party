13a6de2a6e175ac1aa5f0fa5139801dba79cbca8
Lauren Janicki <lauren.a.janicki@gmail.com>
2018-04-18 11:05:59 -0700

    Completed BOOL-Party exercise

--- BOOL-party/main.m
+++ BOOL-party/main.m
@@ -1,17 +1,45 @@
 //
 //  main.m
-//  BOOL-party
+//  BOOL Party
 //
-//  Created by Lauren Janicki on 4/18/18.
+//  Created by Lauren Janicki on 4/17/18.
 //  Copyright © 2018 Lauren Janicki. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+BOOL areIntsDifferent (int thing1, int thing2)
+{
+    if (thing1 == thing2)
+    {
+        return (NO);
     }
-    return 0;
+    else
+    {
+        return (YES);
+    }
+}
+
+
+NSString *boolString (BOOL yesNo)
+{
+    if (yesNo == NO)
+    {
+        return (@"NO");
+    }
+    else
+    {
+        return (@"YES");
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    BOOL areTheyDifferent = areIntsDifferent(5, 5);
+    NSLog(@"Are %d and %d different? %@", 5, 5, boolString(areTheyDifferent));
+    
+    areTheyDifferent = areIntsDifferent(23, 42);
+    NSLog(@"are %d and %d different? %@", 23, 42, boolString(areTheyDifferent));
+    
+    return (0);
 }
