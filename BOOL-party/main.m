525eff3280e9dd210ee83d77032d3a366d43e138
Lauren Janicki <lauren.a.janicki@gmail.com>
2018-04-18 10:53:40 -0700

    Initial Commit

---
+++ BOOL-party/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BOOL-party
+//
+//  Created by Lauren Janicki on 4/18/18.
+//  Copyright © 2018 Lauren Janicki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
